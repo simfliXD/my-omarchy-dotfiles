94cc77523120c50ebb0e1fe5cfbfa65315f9ed88
Simon Flink <flinksimon@hotmail.com>
2026-09-16 21:12:12 +0200

    Update autostart.lua

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -2,5 +2,5 @@
 -- o.launch_on_start("my-service")
 
 
-o.launch_on_start("omarchy-launch-browser")
-o.launch_on_start("omarchy-launch-tui spotatui")
+-- o.launch_on_start("omarchy-launch-browser")
+-- o.launch_on_start("omarchy-launch-tui spotatui")
